2ad4020e7194db6141936afe7bd1ba6b1eb94e09
ray03j <eihouse.potato@outlook.jp>
2025-01-03 17:18:04 +0900

    feat: 接待モードのAPI

--- router/explanation_settai.py
+++ router/explanation_settai.py
@@ -1,14 +1,12 @@
 import os
 from fastapi import APIRouter
 from pydantic import BaseModel
-# from openai import OpenAI
-import openai
+from openai import OpenAI
 
 router = APIRouter()
 
 class ChatRequest(BaseModel):
-    model: str
-    messages: list
+    dajare: str
 
 @router.post("/settai")
 def chat(request: ChatRequest):
@@ -16,16 +14,26 @@ def chat(request: ChatRequest):
     client = OpenAI(api_key=api_key)
 
     system_content = """
+    あなたは優秀な後輩です。相手が喜ぶ言葉遣いを知っていて、適切な言葉を選ぶことができます。
     """
 
-    response = client.chat.completions.create(
-        model=request.model,
-        messages=[
-            {
-                "role": "system", 
-                "content": system_content
-            },
-            *request.messages
-        ]
-    )
-    return response
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system", 
+                    "content": system_content
+                },
+                {
+                    "role" : "user",
+                    "content": f"「{request.dajare}」というダジャレについて、セリフ調で大げさに150字以内で褒めてください"
+                }
+            ]
+        )
+
+        answer = response.choices[0].message.content
+
+        return {"response": answer}
+    except Exception as e:
+        return {"error": str(e)}
